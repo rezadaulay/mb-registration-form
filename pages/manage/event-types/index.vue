e707628268f00c14fc21d7339f9b16032eb41e66
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2024-12-21 03:06:49 +0700

    add some buttons

--- pages/manage/event-types/index.vue
+++ pages/manage/event-types/index.vue
@@ -12,7 +12,7 @@ import type { Database } from '~~/types/database.types'
 
 const client = useSupabaseClient<Database>()
 
-const { data, error } = await client.from('eventTypes').select('id, title').order('created_at')
+const { data, error } = await client.from('eventTypes').select('id, title, slug').order('created_at')
 if (error) {
   throw createError({ statusMessage: error.message })
 }
@@ -38,7 +38,7 @@ const removeEventType = async (eventType: EventType) => {
             <th class="text-left">
               <strong>Judul Acara</strong>
             </th>
-            <th class="text-left" width="30%">
+            <th class="text-left" width="20%">
               
             </th>
           </tr>
@@ -50,8 +50,11 @@ const removeEventType = async (eventType: EventType) => {
           >
             <td><router-link :to="`/manage/registrants/${item.id}`">{{ item.title }}</router-link></td>
             <td class="text-right">
-              <v-btn icon="mdi-pencil" size="x-small" class="mr-2" :to="`/manage/event-types/${item.id}`"></v-btn>
-              <v-btn icon="mdi-trash-can-outline" color="error" size="x-small" @click.prevent="removeEventType(item)"></v-btn>
+              <div class="pt-2 pb-2">
+                <v-btn icon="mdi-share-all" :to="`/${item.slug}`" target="_blank" size="x-small" color="info" class="mb-2"></v-btn><br>
+                <v-btn icon="mdi-pencil" size="x-small" class="mb-2" :to="`/manage/event-types/${item.id}`"></v-btn><br>
+                <v-btn icon="mdi-trash-can-outline" color="error" size="x-small" @click.prevent="removeEventType(item)"></v-btn>
+              </div>
             </td>
           </tr>
         </tbody>
--- pages/manage/registrants/[eventTypeid].vue
+++ pages/manage/registrants/[eventTypeid].vue
@@ -15,6 +15,21 @@ const showSnackbar = async (message: string) => {
     snackbar.value = true;
 }
 
+const formatDate = (isoString: string | undefined): string => {
+  if (!isoString) {
+    return '';
+  }
+  const date = new Date(isoString);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+  const year = date.getFullYear();
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
+
+  return `${day}-${month}-${year} ${hour}:${minute}`;
+}
+
 // import type EventType from '~~/types/eventType'
 import type { Database } from '~~/types/database.types'
 
@@ -47,7 +62,7 @@ const loadEventType = async () => {
 const showAll: Ref<boolean> = ref(false)
 const registrants: Ref<Array<Registrant> | null> = ref(null)
 const loadRegistrants = async () => {
-  const { data, error } = await client.from('registrants').select('id, name, phone, gender, email, event_type_id, closed').eq('event_type_id', eventType.id).in('closed', showAll.value ? [true, false] : [false]).order('created_at', { ascending: false })
+  const { data, error } = await client.from('registrants').select('id, name, phone, gender, email, event_type_id, closed, created_at').eq('event_type_id', eventType.id).in('closed', showAll.value ? [true, false] : [false]).order('created_at', { ascending: true })
   if (error) {
     throw createError({
       statusCode: 500,
@@ -109,7 +124,7 @@ loadEventType()
             v-for="item in registrants"
             :key="item.id"
           >
-            <td width="40%"><small>{{ item.gender === 'pria' ? 'Ikhwan' : 'Akhwat' }}</small><br>{{ item.name }}<br>{{ item.phone }}</td>
+            <td width="40%"><small>{{ item.gender === 'pria' ? 'Ikhwan' : 'Akhwat' }}</small><br><strong>{{ item.name }}</strong><br><a :href="`https://api.whatsapp.com/send?phone=${item.phone.replace('+', '')}`" target="_blank">{{ item.phone }}</a><br>Daftar: <strong>{{ formatDate(item.created_at) }}</strong></td>
             <!-- <td>{{ item.gender === 'pria' ? 'Ikhwan' : 'Akhwat' }}</td> -->
             <!-- <td>{{ item.phone }}</td> -->
             <td class="text-center"><v-chip :color="item.closed ? 'success' : ''">{{ item.closed ? 'Selesai' : 'Menunggu'}}</v-chip></td>
